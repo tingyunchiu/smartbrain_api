0c6e82826effe8c6183d023b7e2541212a794876
tingyunchiu <c199403@gmail.com>
2021-08-01 00:42:49 +0800

    redesigned the database

--- server.js
+++ server.js
@@ -1,93 +1,99 @@
-
-const express =  require('express');
+const express = require('express');
 const bodyParser = require('body-parser');
 const cors = require('cors');
-const app =  express ()
+const app = express()
 const knex = require('knex')
 const bcrypt = require('bcryptjs')
 const db = knex({
-  client: 'pg',
-  connection: {
-    host : '127.0.0.1',
-    user : 'postgres',
-    password : 'test',
-    database : 'smartbrain'
-  }
+    client: 'pg',
+    connection: {
+        host: '127.0.0.1',
+        user: 'postgres',
+        password: 'test',
+        database: 'smartbrain'
+    }
 });
 
 app.use(cors())
 app.use(bodyParser.json());
 
 // api/login
-app.post('/api/login',  (req, res) => {
-	db.select('email','hash').from('login').where('email', '=', req.body.email)
-	.then(data => {
-		const isValid = bcrypt.compareSync(req.body.password, data[0].hash)
-		if (isValid) {
-			return db.select('*').from('scores').where('email', '=', req.body.email)
-						.then(user => {
-							res.json(user[0]);
-						})
-						.catch(err => res.status(400).json('unable to get scores'))
-		}else{
-			res.status(400).json('try again')
-		}
-	})
-	.catch (err => res.status(400).json('try again code'))
+app.post('/api/login', (req, res) => {
+    db.select('*').from('login').where('email', '=', req.body.email)
+        .then(data => {
+            const isValid = bcrypt.compareSync(req.body.password, data[0].hash)
+            if (isValid) {
+                res.json({
+                            uid: data[0].uid,
+                            name: data[0].name,
+                            email: data[0].email
+                        });
+            } else {
+                res.status(400).json('try again')
+            }
+        })
+        .catch(err => res.status(400).json('try again'))
 })
 
 // api/signup
-app.post('/api/signup',  (req, res) => {
- 	const {name, email, password} = req.body
- 	const hash = bcrypt.hashSync(password, 10)
+app.post('/api/signup', (req, res) => {
+  const { email, name, password } = req.body;
+  const hash = bcrypt.hashSync(password);
+  const date = new Date()
 
- 	db.transaction(trx =>{
-		trx.insert({email: email, hash: hash })
- 	.into('login')
- 	.returning('email')
- 	.then(loginEmail =>{
- 		return trx('scores')
- 						.returning('*')
- 						.insert({
- 							email: loginEmail[0],
- 							name: name,
- 							joined: new Date()
- 						})
- 						.then(user => {
-							res.json(user[0])
- 						})
-
- 	})
- 	.then(trx.commit)
- 	.catch(trx.rollback)
-  })
- 	.catch(err => res.status(404).json('unable to join'))
+    db.transaction(trx => {
+      trx.insert({
+        hash: hash,
+        email: email,
+        name: name,
+        joined: date
+      })
+      .into('login')
+      .returning('uid')
+      .then(uid => {
+        return trx('scores')
+          .returning('*')
+          .insert({
+            uid: uid[0],
+            date: date
+          })
+          .then(user => {
+            res.json(user[0]);
+          })
+      })
+      .then(trx.commit)
+      .catch(trx.rollback)
+    })
+    .catch(err => res.status(400).json('unable to sign up'))
 })
 
-// api/home/:id
-app.get('/api/home/:id', (req, res) => {
-	const {id} = req.params;
-	
+// api/home/:uid
+app.get('/api/home/:uid', (req, res) => {
+    const { uid } = req.params;
+    db.select('scores').from('scores').where('uid', '=', uid)
+        .then(data => {
+            res.json(data.map(item =>item.scores))
+        })
+    .catch(err => res.status(400).json('unable to get recorders'))
 })
 
 // api/scores
 app.post('/api/scores', (req, res) => {
-	const {name, email, scores} = req.body;
-		db('scores')
-		.returning('*')
-		.insert({
-			name: name,
-			email: '',
-			scores: scores,
-			joined: new Date()
-		})
-		.then(user => {
-			res.json(user[0])
-		})
+    const { uid, scores } = req.body;
+    db('scores')
+        .returning('*')
+        .insert({
+            uid: uid,
+            scores: scores,
+            date: new Date()
+        })
+        .then(user => {
+            res.json(user[0])
+        })
+        .catch(err => res.status(404).json('unable to add scores'))
 })
 
 
-app.listen(3001, ()=> {
-  console.log('app is running on port 3001');
+app.listen(3001, () => {
+    console.log('app is running on port 3001');
 })
-
