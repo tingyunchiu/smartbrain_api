1d03b21272710bcd60341e86d1d9d3509de14107
tingyunchiu <c199403@gmail.com>
2021-08-01 18:07:40 +0800

    connect to heroku db

--- server.js
+++ server.js
@@ -6,11 +6,9 @@ const knex = require('knex')
 const bcrypt = require('bcryptjs')
 const db = knex({
     client: 'pg',
-    connection: {
-        host: '127.0.0.1',
-        user: 'postgres',
-        password: 'test',
-        database: 'smartbrain'
+    connectionString: process.env.DATABASE_URL,
+    ssl: {
+        rejectUnauthorized: false
     }
 });
 
